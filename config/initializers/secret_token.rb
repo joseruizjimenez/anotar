a2a785e97d70777493323a88c3d1add11ea2857e
Jose Ruiz Jimenez <joseruizjimenez@gmail.com>
2012-07-10 11:50:35 +0200

    Rails framwork ready

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Anotar::Application.config.secret_token = '8ecb3b301a9b48c0b2e1893022189673d66633d33912fa772495cbdf972feebb466dec282b2ed467a82b8c8209a71dd941777053425a70732d7968a8a2d81c9d'
