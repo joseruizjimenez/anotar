847928c5670e39f36dc66b0d3af7b5c5f5ac9d35
Jose Ruiz Jimenez <joseruizjimenez@gmail.com>
2012-07-21 19:54:58 +0200

    Installed and configured Devise for future user management

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,22 @@ Anotar::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.default :charset => "utf-8"
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "anotar.heroku.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
