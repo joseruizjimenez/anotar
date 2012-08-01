d4331529993a1c2c59d0f9cef3a81a26f4105a39
Jose Ruiz Jimenez <joseruizjimenez@gmail.com>
2012-08-01 03:15:55 +0200

    Changed some migrations default boolean type... for PG

--- db/migrate/20120724131951_add_edit_count_and_public_to_notes.rb
+++ db/migrate/20120724131951_add_edit_count_and_public_to_notes.rb
@@ -1,8 +1,8 @@
 class AddEditCountAndPublicToNotes < ActiveRecord::Migration
   def change
     add_column :notes, :edit_count, :integer, :default => 0
-    add_column :notes, :shared, :boolean, :default => 0
-    add_column :notes, :fav, :boolean, :default => 0
+    add_column :notes, :shared, :boolean, :default => false
+    add_column :notes, :fav, :boolean, :default => false
     add_column :notes, :html_text, :text
   end
 end
