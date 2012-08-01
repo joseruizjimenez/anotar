a11e74f4fffb0a29a75c20f986e1a42c26053dc3
Jose Ruiz Jimenez <joseruizjimenez@gmail.com>
2012-08-01 11:28:42 +0200

    More changes to email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Anotar::Application.configure do
   #ActionMailer::Base.smtp_settings[:enable_starttls_auto] = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
   #config.action_mailer.smtp_settings = {
   #  address: "smtp.gmail.com",
@@ -85,3 +85,4 @@ Anotar::Application.configure do
   #  password: ENV["GMAIL_PASSWORD"]
   #}
 end
+# ActionMailer::Base.smtp_settings[:enable_starttls_auto] = false
